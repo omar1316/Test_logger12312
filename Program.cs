eb99a26187ee1e560833a94409842c124d64aa0f
Omar Asala <149591343+omar1316@users.noreply.github.com>
2026-02-17 21:23:52 +0200

    test_logger

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var loger = new TestService(new LoggerFactory().CreateLogger<TestService>());
+            loger.DoWork();
+        }
+    }
+}
---
+++ Testservice.cs
@@ -0,0 +1,29 @@
+﻿using log4net.Core;
+using Microsoft.Build.Framework;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Logger
+{
+    public class TestService
+    {
+        private readonly ILogger<TestService> _logger;
+
+        public TestService(ILogger<TestService> logger)
+        {
+            _logger = logger;
+        }
+        public void DoWork()
+        {
+            _logger.LogInformation("I started working");
+
+            _logger.LogWarning("This is just a warning");
+
+            _logger.LogError("Something went wrong");
+        }
+    }
+}
